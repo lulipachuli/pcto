b20490d2414e04676d36e2cdf4482ca301979a37
lulipachuli <124669212+lulipachuli@users.noreply.github.com>
2023-02-07 12:04:30 +0100

    Add files via upload

---
+++ amo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a = 17;
+    int b = 3;
+    int divisione = 17 / 3;
+    int divisioneresto = 17 % 3;
+    printf("17/3 :  %d\n", divisione);
+    printf("17%%3 : %d\n", divisioneresto);
+
+}
