dca1a25ca1315446b2dcd2ebd36161aa05d7535b
lulipachuli <124669212+lulipachuli@users.noreply.github.com>
2023-02-07 17:02:15 +0100

    Add files via upload

---
+++ bisestile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a;
+    printf("inserisci un numero: \n");
+    scanf("%d", &a);
+    printf("a: %d\n", a);
+    if (a%100 != 0 (&&), a%4==0 || a%400==0)
+    {
+       printf("è bisestile \n");
+    }
+    else
+    {
+        printf("non è bisestile \n");
+    }
+}
