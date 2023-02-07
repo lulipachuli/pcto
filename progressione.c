1e5a709f5db5da98f07573faef7160e8977f4b1e
lulipachuli <124669212+lulipachuli@users.noreply.github.com>
2023-02-07 16:45:22 +0100

    Add files via upload

---
+++ progressione.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{ 
+    int a;
+    int b;
+    int c;
+    printf ("inserisci tre numeri: \n");
+    scanf("%d %d %d", &a, &b, &c);
+    printf("a: %d\n b: %d\n c: %d\n", a, b, c);
+    if (b - a == c - b)
+    {
+       printf("è una progressione aritmetica \n");
+    }
+    else
+    {
+        printf("non è una progressione aritmetica \n");
+    }
+}
