59ed6003a6db40b663192ef3011ba80f419886e0
lulipachuli <124669212+lulipachuli@users.noreply.github.com>
2023-02-09 12:30:22 +0100

    Add files via upload

---
+++ potenza2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main ()
+{
+float base;
+float esponente;
+float potenza = 1;
+printf ("inserisci due mureri; \n");
+scanf  ("%f %f", &base, &esponente);
+
+if (esponente > 0);
+{
+    while(esponente != 0)
+    {
+        potenza = potenza * base;
+        esponente = esponente - 1;
+    }
+    printf ("%f \n", potenza);
+}
+else if(esponente < 0)
+{
+    while(esponente != 0)
+    {
+        potenza = potenza / base;
+        esponente = esponente + 1;
+    }
+    printf ("%f \n", potenza);
+}
+else
+{
+  printf("1 \n");
+}
+}
